7dae21524068a3889b5a5b1a2b4157270a9a7a7a
Nikhil Prajapati <112732049+Nikhil127001@users.noreply.github.com>
2023-05-31 04:24:37 +0530

    DSA JAVA DAY 54

--- TriesB.java
+++ TriesB.java
@@ -1,62 +1,238 @@
 public class TriesB {
 
-    // creating nodes for trie
-    static class Node{
-        Node childrens[] = new Node[26];
-        boolean eow = false;
+  // creating nodes for trie
+  static class Node {
+    Node childrens[] = new Node[26];
+    boolean eow = false;
 
-        Node(){// creating all childrens of root as null in the begining
-            for(int i =0; i< childrens.length; i++){
-                childrens[i] = null;
-            }
-        }
+    Node() {// creating all childrens of root as null in the begining
+      for (int i = 0; i < childrens.length; i++) {
+        childrens[i] = null;
+      }
     }
-    public static Node root = new Node();
+  }
 
-    public static void insert(String str){// T.C. -> O(L)
-        // initialising the root as curr
-        Node curr = root;
-        // traverse the word and insert it in the arr of Nodes 
-        for(int level = 0; level< str.length(); level++){
-            int idx = str.charAt(level) - 'a';
-            // check ifthe childrens of root contains the char
-            if( curr.childrens[idx] == null){
-                // insert
-                curr.childrens[idx] = new Node();
-            }
-            // at last change current
-            curr = curr.childrens[idx];
-        }
-        curr.eow = true;
+  public static Node root = new Node();
+
+  public static void insert(String str) {// T.C. -> O(L)
+    // initialising the root as curr
+    Node curr = root;
+    // traverse the word and insert it in the arr of Nodes
+    for (int level = 0; level < str.length(); level++) {
+      int idx = str.charAt(level) - 'a';
+      // check ifthe childrens of root contains the char
+      if (curr.childrens[idx] == null) {
+        // insert
+        curr.childrens[idx] = new Node();
+      }
+      // at last change current
+      curr = curr.childrens[idx];
     }
+    curr.eow = true;
+  }
 
-    public static boolean search(String str){// T.C. -> O(L)
-        // initialising the root as curr
-        Node curr = root;
-        // traverse the word and insert it in the arr of Nodes 
-        for(int level = 0; level< str.length(); level++){
-            int idx = str.charAt(level) - 'a';
-            // check ifthe childrens of root contains the char
-            if( curr.childrens[idx] == null){
-                // return false
-                return false;
-            }
-            // at last change current
-            curr = curr.childrens[idx];
-        }
-        if(curr.eow == true){
-            return true;
-        }
+  public static boolean search(String str) {// T.C. -> O(L)
+    // initialising the root as curr
+    Node curr = root;
+    // traverse the word and insert it in the arr of Nodes
+    for (int level = 0; level < str.length(); level++) {
+      int idx = str.charAt(level) - 'a';
+      // check ifthe childrens of root contains the char
+      if (curr.childrens[idx] == null) {
+        // return false
         return false;
+      }
+      // at last change current
+      curr = curr.childrens[idx];
     }
-    public static void main (String[] args){
-     String arr[] = {"their", "there", "the", "a", "ask", "thee"};
-    // insert function for the trie
-     for(int i =0; i<arr.length; i++){
-        insert(arr[i]);
-     }
-     // search function for the tree
-     System.out.println(search("their"));
-     System.out.print(search("ther"));
+    if (curr.eow == true) {
+      return true;
     }
+    return false;
+  }
+
+  public static boolean wordbreak(String str) {
+    // base case
+    if (str.length() == 0) {
+      return true;
+    }
+    // solving using substring approach
+    int i = 0;
+    for (int j = 1; j < str.length(); j++) {
+      // substring 1
+      // String one = str.substring(i,j); // last index element not included
+      // String two = str.substring(j); // from j to last of the string
+      // search if the substring one is not in the array
+      if (search(str.substring(0, j)) && wordbreak(str.substring(j))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  static class Node2 {
+    Node2 children[] = new Node2[26];
+    boolean eow = false;
+    int frequency;
+
+    public Node2() {
+      for (int i = 0; i < children.length; i++) {
+        children[i] = null;
+      }
+      frequency = 1;
+    }
+  }
+
+  // creating root node
+  public static Node2 root2 = new Node2();
+
+  // creating function to Insert new child node to the array
+  public static void Insert2(String str) {
+    // initialising root node as curr
+    Node2 curr = root2;
+    // treverse the string characters one by one and add them into the trie
+    for (int i = 0; i < str.length(); i++) {
+      // store first character of the string
+      char ch = str.charAt(i);
+      // the character place in the childrens array is null then create new node for
+      // the element
+      if (curr.children[ch - 'a'] == null) {
+        curr.children[ch - 'a'] = new Node2();
+      } else {
+        curr.children[ch - 'a'].frequency++;
+      }
+      // change curr to children[ch - 'a'] Node
+      curr = curr.children[ch - 'a'];
+    }
+    // and at last change end of eord to true
+    curr.eow = true;
+  }
+
+  public static void FindUniquePrefix(Node2 root2, String st) {
+
+    // if root is null return -> base case
+    if (root2 == null) {
+      return;
+    }
+
+    if (root2.frequency == 1) {
+      System.out.println(st);
+      return;
+    }
+
+    // check for each node in the path to word end
+    for (int i = 0; i < root2.children.length; i++) {
+      if (root2.children[i] != null) {
+        FindUniquePrefix(root2.children[i], st + (char) (i + 'a'));
+      }
+    }
+  }
+
+  public static boolean searchPrefix(String str) {
+    for (int i = 0; i < str.length(); i++) {
+      // find current char
+      char ch = str.charAt(i);
+      if (root2.children[ch - 'a'] == null) {
+        return false;
+      }
+      root2 = root2.children[ch - 'a'];
+    }
+    return true;
+  }
+
+  public static int CountUniqueSubs(Node root) {
+    // base case
+    if (root == null) {
+      return 0;
+    }
+    int count = 0;
+    // check in array if for the non null nodes then count the nodes linked to that
+    // perticular non null node
+    for (int i = 0; i < 26; i++) {
+      if (root.childrens[i] != null) {
+        count += CountUniqueSubs(root.childrens[i]);
+      }
+    }
+    // here we want to add the count of self node also
+    return count + 1;
+
+  }
+  public static String st = "";
+  public static void FindLongestString(Node root,String temp){
+    if(root == null){
+      return;
+    }
+    for(int i =0; i< 26; i++){
+      if(root.childrens[i] != null && root.childrens[i].eow == true){
+        temp += (char)(i+'a');
+        if(temp.length() > st.length()){
+          st = temp;
+        }
+        FindLongestString(root.childrens[i],temp);
+        temp = temp.substring(0,temp.length()-1);
+
+      }
+    }
+    // back tracking 
+  }
+
+  public static void main(String[] args) {
+    // String arr[] = {"their", "there", "the", "a", "ask", "thee"};
+    // // insert function for the trie
+    // for(int i =0; i<arr.length; i++){
+    // insert(arr[i]);
+    // }
+    // // search function for the tree
+    // System.out.println(search("their"));
+    // System.out.print(search("ther"));
+
+    // //** IMP */ word break problem
+
+    // String arr[] = {"i","like","samsung","mobile","phone"};
+    // for(int i =0; i<arr.length; i++){
+    // insert(arr[i]);
+    // }
+    // // creating function to check if the string given is in the arr
+    // String str = "ilikesamsung";
+    // System.out.print(wordbreak(str));
+
+    // ** IMP prefix problem
+
+    // String arr2[] ={"duck","dog","dove", "zebra"};
+    // // first insert elements in trie and then find the unique prifix for each
+    // for(int j =0; j< arr2.length; j++){
+    // Insert2(arr2[j]);
+    // }
+    // root2.frequency = -1;
+    // FindUniquePrefix(root2,"");
+
+    // // ** searchPrefix Question
+
+    // String arr2[] ={"apple","app","mango","man","woman"};
+    // // first insert elements in trie and then find the unique prifix for each
+    // for(int j =0; j< arr2.length; j++){
+    // Insert2(arr2[j]);
+    // }
+    // System.out.print(searchPrefix("man")
+    // );
+
+    //// ** IMP count unique Substrings
+
+    // String str = "ababa";
+    // //find all suffix of the string str and insert it in the string
+    // for(int i = 0; i< str.length(); i++){
+    // String suffix = str.substring(i);
+    // insert(suffix);
+    // }
+    // // create a function to count all the unique substrings from str
+    // System.out.print(CountUniqueSubs(root));
+
+     String arr[] = {"a","banana","ap","app","appl","apple","apply","applys"};
+    for(int i =0; i<arr.length; i++){
+    insert(arr[i]);
+    }
+    String str = "";
+    FindLongestString(root,str);
+    System.out.print(st);
+  }
 }
--- practice.java
+++ practice.java
@@ -1,334 +1,164 @@
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.LinkedList;
-import java.util.HashMap;
-
 public class practice {
 
-  static class Node {
-    int data;
-    Node left;
-    Node right;
+//   // creating node class for tries and creating its structure
+//   static class Node {
+//     // creating array of size 26 to store child nodes
+//     Node childrens[] = new Node[26];
+//     // initialising end of word as false
+//     boolean eow = false;
 
-    public Node(int data) {
-      this.data = data;
-      this.left = null;
-      this.right = null;
+//     // creating constructor for Node class
+//     public Node() {
+//       // initialising all the children nodes as null
+//       for (int i = 0; i < childrens.length; i++) {
+//         childrens[i] = null;
+//       }
+//     }
+//   }
+
+//   // creating root node
+//   public static Node root = new Node();
+
+//   // creating function to Insert new child node to the array
+//   public static void Insert(String str){
+//     // initialising root node as curr
+//     Node curr = root;
+  
+//     // treverse the string characters one by one and add them into the trie
+//     for(int i =0; i<str.length(); i++){
+//         // store first character of the string
+//       char ch = str.charAt(i);
+//       // the character place in the childrens array is null then create new node for the element
+//       if(curr.childrens[ch - 'a'] == null){
+//         curr.childrens[ch - 'a'] = new Node();
+//       }
+//       //change curr to children[ch - 'a'] Node
+//         curr = curr.childrens[ch - 'a'];
+//     }
+//     // and at last change end of eord to true
+//     curr.eow = true;
+//   }
+//   // creating function to search in the trie
+//   public static boolean Search(String str){
+//     Node curr = root;
+//     for(int i = 0; i< str.length(); i++ ){
+//       char ch = str.charAt(i);
+//       // chreck if the element is not in the children array
+//       //or
+//       // the children at the index is null then return false
+//       if(curr.childrens[ch - 'a'] == null){
+//         return false;
+//       }
+//       curr = curr.childrens[ch - 'a'];
+//     }
+//     // check if the end of word is true 
+//     if(curr.eow == true){
+//       return true;
+//     }
+//     return false;
+//   }
+//   public static boolean wordbreak(String str){
+//     // base case 
+//     if(str.length() == 0){
+//         return true;
+//     }
+//     // solving using substring approach
+//      for(int j = 1; j< str.length();j++){
+//         // substring 1
+//     //   String one = str.substring(i,j); // last index element not included 
+//     //   String two = str.substring(j); // from j to last of the string
+//       // search if the substring one is not in the array
+//       if(Search(str.substring(0,j)) && wordbreak(str.substring(j))){
+//         return true;
+//       }
+//      }
+//      return false;
+// }
+
+// creating Node for prefix problem
+ static class Node2 {
+  Node2 children[] = new Node2[26];
+  boolean eow = false;
+  int frequency;
+  public Node2(){
+    for(int i=0; i< children.length; i++){
+      children[i] = null;
     }
+    frequency = 1;
+  }
+ }
+  // creating root node
+  public static Node2 root = new Node2();
 
+  // creating function to Insert new child node to the array
+  public static void Insert2(String str){
+    // initialising root node as curr
+    Node2 curr = root;
+    // treverse the string characters one by one and add them into the trie
+    for(int i =0; i<str.length(); i++){
+        // store first character of the string
+      char ch = str.charAt(i);
+      // the character place in the childrens array is null then create new node for the element
+      if(curr.children[ch - 'a'] == null){
+        curr.children[ch - 'a'] = new Node2();
+      }else{
+        curr.children[ch - 'a'].frequency++;
+      }
+      //change curr to children[ch - 'a'] Node
+        curr = curr.children[ch - 'a'];
+    }
+    // and at last change end of eord to true
+    curr.eow = true;
   }
 
-  public static void levelOrder(Node root) {
-    // ArrayList <Integer> b = new ArrayList<>();
-    if (root == null) {
+
+  public static void FindUniquePrefix(Node2 root, String st){
+    
+    // if root is null return -> base case
+    if(root == null){
       return;
     }
-    Queue<Node> a = new LinkedList<>();
 
-    a.add(root);
-    a.add(null);
+    if(root.frequency == 1){
+      System.out.print(st);
+      return;
+    }
 
-    while (!a.isEmpty()) {
-      Node curr = a.remove();
-      if (curr == null) {
-        System.out.println();
-        if (a.isEmpty()) {
-          break;
-        } else {
-          a.add(null);
-        }
-      } else {
-        System.out.print(curr.data);
-        if (curr.left != null) {
-          a.add(curr.left);
-        }
-        if (curr.right != null) {
-          a.add(curr.right);
-        }
+    // check for each node in the path to word end
+    for(int i = 0; i< root.children.length; i++){
+      if(root.children[i] != null){
+        FindUniquePrefix(root.children[i], st+=(char)(i+'a'));
       }
     }
   }
+ 
 
-  public static boolean CheckUnivalued(Node root, int x) {
-    if (root == null) {
-      return true;
-    }
 
-    if (root.data != x) {
-      return false;
-    }
-    boolean left = CheckUnivalued(root.left, x);
-    boolean right = CheckUnivalued(root.right, x);
+  public static void main(String args[]) {
+    // String arr[] = { "the", "their", "there", "add", "tree", "a", "sout" };
 
-    if (left && right) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+    // for (int i = 0; i < arr.length; i++) {
+    //   // insert all the elements in the trie one by one
+    //   Insert(arr[i]);
+    // }
+    // System.out.print( Search("sout"));
 
-  public static void FindMirrorImage(Node root) {
-    if (root == null) {
-      return;
-    }
-    Node temp = root.left;
-    root.left = root.right;
-    root.right = temp;
-    FindMirrorImage(root.left);
-    FindMirrorImage(root.right);
-  }
 
-  public static boolean CheckMI(Node root1, Node root2) {
+    //  // word break problem
+    //  String arr[] = {"i","like","samsung","mobile","phone"};
+    //  for(int i =0; i<arr.length; i++){
+    //         Insert(arr[i]);
+    //      }
+    //  // creating function to check if the string given is in the arr
+    //  System.out.print(wordbreak("ilikesamsung"));
 
-    if (root1 == null && root2 == null) {
-      return true;
-    }
-
-    if (root1.data == root2.data) {
-      return true;
-    }
-
-    boolean left = CheckMI(root1.left, root2.left);
-    boolean right = CheckMI(root1.right, root2.right);
-
-    if (left || right) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  public static Node removeLeafes(Node root, int x) {
-    if (root == null) {
-      return null;
-    }
-
-    root.left = removeLeafes(root.left, x);
-    root.right = removeLeafes(root.right, x);
-
-    if (root.left == null && root.right == null && root.data == x) {
-
-      return null;
-    }
-    return root;
-
-  }
-
-  public static String findAllDups(Node root, HashMap<String, Integer> m) {
-    String str = "";
-    if (root == null) {
-      return "";
-    }
-
-    str += findAllDups(root.left, m);
-    str += Integer.toString(root.data);
-    str += findAllDups(root.right, m);
-
-    // if the hashmap contains key and its value is equal to 1 then print the data
-    // of the root in str you want to add in map in next step
-    if (m.get(str) != null && m.get(str) == 1) {
-      System.out.print(root.data);
-    }
-    // if hashmap contains key str then put it in the hashmap and updateits value to
-    // val + 1
-    if (m.containsKey(str)) {
-      m.put(str, m.get(str) + 1);
-      // if it doesnot contains key str then add it with its value is equal to 1
-    } else {
-      m.put(str, 1);
-    }
-
-    return str;
-  }
-
-  public static void printAllDups(Node root) {
-    // using hashmap because hashMap contains Unique keys with their last updataed
-    // values
-    HashMap<String, Integer> m = new HashMap<>();
-    findAllDups(root, m);
-  }
-
-  public static int FindMaxPath(Node root, int MaxPath) {
-    if (root == null) {
-      return 0;
-    }
-
-    int LeftSum = FindMaxPath(root.left, MaxPath);
-    int rightSum = FindMaxPath(root.right, MaxPath);
-    int totPathSum = LeftSum + root.data + rightSum;
-    MaxPath = Math.max(MaxPath, totPathSum);
-    System.out.print(MaxPath);
-    return totPathSum;
-  }
-
-  static class Res {
-    public int val;
-  }
-
-  static class Solution {
-    Node root;
-
-    int findMaxUtil(Node node, Res res) {
-      if (node == null)
-        return 0;
-      int l = findMaxUtil(node.left, res);
-      int r = findMaxUtil(node.right, res);
-      int max_single = Math.max(Math.max(l, r) + node.data,
-          node.data);
-      int max_top = Math.max(max_single, l + r + node.data);
-      res.val = Math.max(res.val, max_top);
-      return max_single;
-    }
-
-    int findMaxSum() {
-      return findMaxSum(root);
-    }
-
-    int findMaxSum(Node node) {
-      Res res = new Res();
-      res.val = Integer.MIN_VALUE;
-      findMaxUtil(node, res);
-      return res.val;
-    }
-  }
-
-  public static void main(String[] args) {
-    // Question 1 :
-    // Check if a Binary Tree is univalued or not
-    // We have a binary tree, the task is to check if the binary tree is univalued
-    // or not. If found to be
-    // true, then print “YES”. Otherwise, print “NO”.
-    // Sample Input 1 :
-    // Sample Output 1 : false
-
-    // Sol ->
-    // Node root = new Node(1);
-    // root.left = new Node(1);
-    // root.right = new Node(1);
-    // root.left.left = new Node(1);
-    // root.left.right = new Node(1);
-    // root.right.right = new Node(1);
-    // root.right.left = new Node(1);
-
-    // int x = root.data;
-    // System.out.print(CheckUnivalued(root, x));
-
-    // Question 2 :
-    // Invert Binary Tree
-    // Mirror of a Tree: Mirror of a Binary Tree T is another Binary Tree M(T) with
-    // left and right
-    // children of all non-leaf nodes interchanged.
-    // Input1 : fig. above
-    // Output1 : Yes
-
-    // Sol ->
-
-    // Node root = new Node(1);
-    // root.left = new Node(2);
-    // root.right = new Node(3);
-    // root.left.left = new Node(4);
-    // root.left.right = new Node(5);
-    // root.right.right = new Node(7);
-    // root.right.left = new Node(6);
-
-    // levelOrder(root);
-
-    // FindMirrorImage(root);
-
-    // levelOrder(root);
-
-    //// check if two binary trees are mirror images of each other
-
-    // // Btree 1
-    // Node root = new Node(1);
-    // root.left = new Node(2);
-    // root.right = new Node(3);
-    // root.left.left = new Node(4);
-    // root.left.right = new Node(5);
-    // root.right.right = new Node(7);
-    // root.right.left = new Node(6);
-
-    // // Btree2
-    // Node root1 = new Node(1);
-    // root1.left = new Node(3);
-    // root1.right = new Node(2);
-    // root1.left.left = new Node(8);
-    // root1.left.right = new Node(6);
-    // root1.right.left = new Node(5);
-    // root1.right.right = new Node(4);
-
-    // System.out.print(CheckMI(root,root1));
-
-    // Question 3 :
-    // Delete leaf nodes with values as x
-    // We have a binary tree and a target integer x, delete all the leaf nodes
-    // having value as x. Also,
-    // delete the newly formed leaves with the target value as x.
-    // :
-
-    // Node root1 = new Node(1);
-    // root1.left = new Node(3);
-    // root1.right = new Node(3);
-    // root1.left.left = new Node(3);
-    // root1.left.right = new Node(2);
-
-    // int n =3;
-    // removeLeafes(root1, n);
-    // levelOrder(root1);
-
-    // **IMP */ Question 4 :
-    // Find All Duplicate Subtrees
-    // We have a binary tree, find all duplicate subtrees. For each duplicate
-    // subtree, we only need to
-    // return the root node of any one of them. Two trees are duplicates if they
-    // have the same
-    // structure with the same node values.
-    // Input1:
-    // 1
-    // / \
-    // 4 3
-    // / / \
-    // 3 4 3
-    // /
-    // 3
-    // Output1: 4-3 , 3
-
-    // Node root = null;
-    // root = new Node(1);
-    // root.left = new Node(4);
-    // root.right = new Node(3);
-    // root.left.left = new Node(3);
-    // root.right.left = new Node(4);
-    // root.right.left.left = new Node(3);
-    // root.right.right = new Node(3);
-    // printAllDups(root);
-
-    // Question 5 :
-    // Maximum Path Sum in a Binary Tree
-    // We have a binary tree, find the maximum path sum. The path may start and end
-    // at any node in
-    // the tree.
-    // Input1:
-    // kumarnikhil2111@gmail.com
-    // 4
-    // / \
-    // 2 7
-    // Output1: 13
-    // Input2:
-    // ;
-    // Output2: 42
-
-    Solution tree = new Solution();
-    tree.root = new Node(10);
-    tree.root.left = new Node(2);
-    tree.root.right = new Node(10);
-    tree.root.left.left = new Node(20);
-    tree.root.left.right = new Node(1);
-    tree.root.right.right = new Node(-25);
-    tree.root.right.right.left = new Node(3);
-    tree.root.right.right.right = new Node(4);
-    System.out.println("maximum path sum is : " +
-        tree.findMaxSum());
-
+     // prefix problem
+     String arr[] ={"duck","dog","dove", "zebra"};
+      // first insert elements in trie and then find the unique prifix for each
+      for(int j =0; j< arr.length; j++){
+        Insert2(arr[j]);
+       }
+       root.frequency = -1;
+        FindUniquePrefix(root,"");  
   }
 }
