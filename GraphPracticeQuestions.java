6586dc84fafade05f6ee0c0fb1283bc6efe6f29b
Nikhil Prajapati <112732049+Nikhil127001@users.noreply.github.com>
2023-06-07 23:49:12 +0530

    DSA JAVA DAY 59

--- GraphPracticeQuestions.java
+++ GraphPracticeQuestions.java
@@ -1,23 +1,35 @@
 import java.util.*;
 import java.util.LinkedList;
+import java.util.Arrays;
+
 public class GraphPracticeQuestions {
-    static class Edge{
-        int str;
-        int dest;
-        int wt;
-        public Edge(int start, int destination, int weight){
-            this.str = start;
-            this.dest = destination;
-            this.wt = weight;
+
+    // ...........................................................................
+    static void addEdge(ArrayList<Integer> graph[], int u, int v) {
+        graph[u].add(v);
+        graph[v].add(u);
+    }
+
+    public static void createGraph(ArrayList<Integer> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
         }
     }
 
-    public static boolean CycleExists(ArrayList<Edge> graph[]){
+    public static void isCycleExists(ArrayList<Integer> graph[]) {
+        if (CycleDetectionBFS(graph)) {
+            System.out.print("Cycle exists");
+        } else {
+            System.out.print("Cycle not exists");
+        }
+    }
+
+    public static boolean CycleDetectionBFS(ArrayList<Integer> graph[]) {
         boolean visited[] = new boolean[graph.length];
 
-        for(int i = 0; i<graph.length; i++){
-            if(!visited[i]){
-                if(CycleExistsUtils(graph,visited,i,-1)){
+        for (int i = 0; i < graph.length; i++) {
+            if (!visited[i]) {
+                if (CycleDetectionBFSUtils(graph, i, visited)) {
                     return true;
                 }
             }
@@ -25,45 +37,65 @@ public class GraphPracticeQuestions {
         return false;
     }
 
-    public static boolean CycleExistsUtils(ArrayList<Edge> graph[] , boolean visited[] , int start,int parent){
+    public static boolean CycleDetectionBFSUtils(ArrayList<Integer> graph[], int start, boolean visited[]) {
+        int flag[] = new int[graph.length];
+        // first initialise all the element of the array as -1 (means unvisited)
+        Arrays.fill(flag, -1);
+        // then add element in the Queue and change flag to 0 (means added to Queue)
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        flag[start] = 0;
 
-        int curr = start;
-        visited[curr] = true;
-     
-                for(int j = 0 ; j<graph[curr].size();j++){
-                 Edge e = graph[curr].get(j);
-                 if(!visited[e.dest]){
-                    if(CycleExistsUtils(graph,visited,e.dest,curr)){
-                    return true;
-                 }
+        while (!q.isEmpty()) {
+            // then poll element from the Queue and change flag to -1 (means visited or
+            // element removed from the Queue)
+            int curr = q.poll();
+            flag[curr] = -1;
+            if (!visited[curr]) {
+                // if element is not visited then change it to visited
+                visited[curr] = true;
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    int e = graph[curr].get(i);
+                    // if the element is not visited and its flag is equal to 0 that means cycle
+                    // exists
+                    if (flag[e] == 0) {
+                        return true;
+                    }
+                    // if the element is not visited and its neighbour flag is not equal to 0 than
+                    // add the neighbour to the queue and change flag to 0;
+                    q.add(e);
+                    flag[e] = 0;
                 }
-                 else if(e.dest != parent && visited[e.dest]){
-                    return true;
-                 }
-           
-    }
-    return false;
-}
-    public static void main(String args[]){
-        // creating graph
-        // @SuppressWarnings("unchecked");
-        int size = 5;
-        ArrayList<Edge> Graph[] = new ArrayList[size];
-
-        for(int i = 0; i<size;i++){
-            Graph[i] = new ArrayList<>();
+            }
         }
+        return false;
+    }
+    // ...........................................................................
 
-        Graph[0].add(new Edge(0,1,1));
-        Graph[0].add(new Edge(0,2,1));
-        Graph[1].add(new Edge(1,0,1));
-        Graph[1].add(new Edge(1,2,1));
-        // Graph[2].add(new Edge(2,0,1));
-        // Graph[2].add(new Edge(2,1,1));
-        
+    public static void main(String args[]) {
+        //// Question 1 :
+        // Detect cycle in an undirected graph using BFS
+        // We have an an undirected graph, how to check if there is a cycle in the
+        // graph? For example,
+        // the following graph has a cycle 1-0-2-1..
 
-        System.out.print(CycleExists(Graph));
-        
+        //// Cycle detection in undirected graphs using BFS
+        int size = 4;
+        ArrayList<Integer> Graph[] = new ArrayList[size];
+        createGraph(Graph);
+        addEdge(Graph, 0, 1);
+        addEdge(Graph, 1, 2);
+        addEdge(Graph, 2, 0);
+        addEdge(Graph, 2, 3);
+        isCycleExists(Graph);
+
+        // Question 2 :
+        // Find Minimum Depth of a Binary Tree
+        // We have a binary tree, find its minimum depth. The minimum depth is the
+        // number of nodes along
+        // the shortest path from the root node down to the nearest leaf node. For
+        // example, minimum
+        // height of below Binary Tree is 2.
 
     }
 }
