8d6fb372ad004d9b58a99016bff2d9077477397a
Nikhil Prajapati <112732049+Nikhil127001@users.noreply.github.com>
2023-07-10 01:25:32 +0530

    DSA JAVA DAY 65

--- DynamicProgrammingB.java
+++ DynamicProgrammingB.java
@@ -56,21 +56,87 @@ public class DynamicProgrammingB {
             dp[0][j] = 0;
         }
 
-        for (int i = 1; i < n+1; i++) {
-            for (int j = 1; j < W+1; j++) {
-                 int v = val[i-1];
-                int w = wt[i-1];
-                if(j >= w){ // valid
-                int include = v + dp[i-1][j-w];
-                int exclude = dp[i-1][j];
-                dp[i][j] = Math.max(include,exclude);
-                }else{// invalid
-                 dp[i][j] = dp[i-1][j];
-                }       
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                int v = val[i - 1];
+                int w = wt[i - 1];
+                if (j >= w) { // valid
+                    int include = v + dp[i - 1][j - w];
+                    int exclude = dp[i - 1][j];
+                    dp[i][j] = Math.max(include, exclude);
+                } else {// invalid
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
         }
-    }
+        for (int i = 0; i < dp.length; i++) {
+        for (int j = 0; j < dp[0].length; j++) {
+        System.out.print(dp[i][j]+" ");
+        }
+        System.out.println();
+        }
         return dp[n][W];
     }
+
+    // Target Sum Subset
+
+    public static boolean TargetSumSubset(int num[], int sum){
+        // creating dp array to store answers
+        int size = num.length;
+        boolean dp[][] = new boolean[size+1][sum+1];
+
+        // initialise condition -> if sum is zero then there is only one empty subset so
+        for(int i = 0; i<dp.length; i++){
+            dp[i][0] = true;
+        }
+
+        for(int i = 1 ; i<dp.length; i++ ){
+            for(int j = 1; j< dp[0].length; j++){
+                int n = num[i-1];
+                // include
+                if( j>= n && dp[i-1][j-n]== true){
+                     dp[i][j] = true;
+                }else if(dp[i-1][j] == true){// exclude
+                     dp[i][j] = true;
+                }
+            }
+        }
+        return dp[size][sum];
+    }
+
+    // Unbounded Knapsack problem
+    public static int UnboundedKnapsackTabulation(int[] val, int wt[], int W) {
+        int n = val.length;
+        int dp[][] = new int[n + 1][W + 1];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < dp[0].length; j++) {
+            dp[0][j] = 0;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < W + 1; j++) {
+                int v = val[i - 1];
+                int w = wt[i - 1];
+                if (j >= w) { // valid
+                    int include = v + dp[i][j - w];
+                    int exclude = dp[i - 1][j];
+                    dp[i][j] = Math.max(include, exclude);
+                } else {// invalid
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        for (int i = 0; i < dp.length; i++) {
+        for (int j = 0; j < dp[0].length; j++) {
+        System.out.print(dp[i][j]+" ");
+        }
+        System.out.println();
+        }
+        return dp[n][W];
+    }
+
 
     public static void main(String[] args) {
         int val[] = { 15, 14, 10, 45, 30 };
@@ -89,14 +155,17 @@ public class DynamicProgrammingB {
             }
         }
         System.out.println(KnapsackMemorization(val, wt, W, n, dp));
-
-        // for (int i = 0; i < dp.length; i++) {
-        // for (int j = 0; j < dp[0].length; j++) {
-        // System.out.print(dp[i][j]+" ");
-        // }
-        // System.out.println();
-        // }
-
         System.out.println(KnapsackTabulation(val, wt, W));
+
+        // Target Sum Subset Question
+
+        int number[] = {4,2,7,1,3};
+        int Sum = 10;
+
+        System.out.print(TargetSumSubset(number,Sum));
+
+        // Unbounded Knapsack
+        System.out.println(UnboundedKnapsackTabulation(val, wt, W));
+
     }
 }
